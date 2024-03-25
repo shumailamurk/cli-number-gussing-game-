49ce658f1341394ac62abb8cdca509c660f91fcd
Dr Shumaila Murk <148242038+shumailamurk@users.noreply.github.com>
2024-03-25 13:57:45 -0700

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,19 @@
+#!/usr/bin/env node
+
+import inquirer from "inquirer";
+
+let randomNumber: number = Math.floor(Math.random()*6 + 1)
+
+const answer= await inquirer.prompt([{
+    name: "Number",
+    type: "number",
+    message: "Guess the number",
+}])
+if (answer.Number === randomNumber)
+{
+    console.log("Congratulations! you won the game")
+}
+else
+ {
+    console.log("Sorry! your guess is wrong")
+}
